59c168272c3cd4f63a9322b5a9dbc7894a866c4a
ytunkara <yuscheb@yahoo.com>
2024-12-14 15:40:10 +0000

    Week 8 Group Practice b Commit

---
+++ myproject/books/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from .models import Book
+
+# Create your views here.
+
+def index(request):
+    books = Book.objects.all()
+    return render(request, 'books/index.html', {'books': books})
